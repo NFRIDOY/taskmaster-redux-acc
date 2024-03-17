86536cee8529230a0e6a8caf315733cb1749dffd
NF RIDOY <nfridoy@gmail.com>
2024-03-17 12:23:26 +0600

    export default

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit"
 
+// // export but not default 
 export const store = configureStore({
     reducer: {},
 })
+// // not exported 
+// const store = configureStore({
+//     reducer: {},
+// })
+
+// // export is default
+export default store;
