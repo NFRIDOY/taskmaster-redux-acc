3f59bdcfc2b4d91d45527825a2af9761ec07a487
NF RIDOY <nfridoy@gmail.com>
2024-03-17 17:51:05 +0600

    ([react-hook-form])

---
+++ src/components/tasks/AddTaskModal.jsx
@@ -0,0 +1,24 @@
+import { useForm } from "react-hook-form";
+import Modal from "../ui/Modal";
+
+
+export default function AddTaskModal({ isOpen, setIsOpen }) {
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data);
+    }
+    return (
+        <Modal isOpen={isOpen} setIsOpen={setIsOpen} title={"Add Task"}>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="flex flex-col gap-3">
+                    <label htmlFor="title">Name</label>
+                    <input type="text" name="" id="title" {...register("name")} />
+                    <label htmlFor="title">Email</label>
+                    <input type="email" name="" id="title" {...register("email")} />
+                    <button type="submit" className="btn bg-violet-500 text-white">Submit</button>
+                </div>
+            </form>
+        </Modal>
+    )
+}
