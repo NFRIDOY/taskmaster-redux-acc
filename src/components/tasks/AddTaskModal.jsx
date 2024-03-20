85f972043b481b16463970ef7c89bd430ddfd486
NF RIDOY <nfridoy@gmail.com>
2024-03-21 01:52:38 +0600

    [ui]

--- src/components/tasks/AddTaskModal.jsx
+++ src/components/tasks/AddTaskModal.jsx
@@ -16,13 +16,17 @@ export default function AddTaskModal({ isOpen, setIsOpen }) {
         console.log("After add tasks= ", tasks);
     }
     return (
-        <Modal isOpen={isOpen} setIsOpen={setIsOpen} title={"Add Task"}>
+        <Modal isOpen={isOpen} setIsOpen={setIsOpen} title={`Total Task ${tasks.length}. Add More`}>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="flex flex-col gap-3">
                     <label htmlFor="title">Title</label>
                     <input type="text" name="" id="title" {...register("title")} />
                     <label htmlFor="email">Email</label>
-                    <input type="email" name="" id="email" {...register("email")} />
+                    <input type="text" name="" id="description" {...register("description")} />
+                    <label htmlFor="email">Deadline</label>
+                    <input type="date" name="" id="Deadline" {...register("Deadline")} />
+                    <label htmlFor="email">Assigned To</label>
+                    <input type="text" name="" id="Assignedto" {...register("Assignedto")} />
                     <button type="submit" className="btn bg-violet-500 text-white">Submit</button>
                 </div>
             </form>
